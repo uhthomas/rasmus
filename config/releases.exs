a30dcf982deace1c2eb79aa239ae55e6abbd5cdb
Thomas <9749173+uhthomas@users.noreply.github.com>
2021-04-14 17:11:54 +0100

    Use runtime configuration

--- config/releases.exs
+++ config/releases.exs
@@ -5,9 +5,9 @@ config :libcluster,
     _: [
       strategy: Elixir.Cluster.Strategy.Kubernetes.DNSSRV,
       config: [
-        namespace: "rasmus",
-        service: "rasmus-headless",
-        application_name: "rasmus",
+        namespace: System.fetch_env!("NAMESPACE"),
+        service: System.fetch_env!("SERVICE"),
+        application_name: System.fetch_env!("APP"),
         polling_interval: 10_000
       ]
     ]
