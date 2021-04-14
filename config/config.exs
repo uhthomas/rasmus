894cb8d53a0adb01d50e267cff864337b16720af
Thomas <9749173+uhthomas@users.noreply.github.com>
2021-04-14 01:18:28 +0100

    use DNSSRV, target headless service

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,7 @@ import Config
 config :libcluster,
   topologies: [
     _: [
-      strategy: Elixir.Cluster.Strategy.Kubernetes.DNS,
-      config: [service: "rasmus", application_name: "rasmus", polling_interval: 10_000]
+      strategy: Elixir.Cluster.Strategy.Kubernetes.DNSSRV,
+      config: [service: "rasmus-headless", application_name: "rasmus", polling_interval: 10_000]
     ]
   ]
